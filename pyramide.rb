619254c56b84af660de33e3f0c38a99aa4bb3d11
antoinelitaise <antoine.lit@gmail.com>
2018-06-27 12:18:28 +0200

    pyramid finish avec hauteur infini

--- pyramide.rb
+++ pyramide.rb
@@ -1,14 +1,15 @@
 puts "Salut, bienvenue dans ma super pyramide ! combien d'etages veux tu ?"
 print "> "
-x = gets.chomp
+x = gets.chomp.to_i
 n = 1
 
-while n <= x.to_i
-    if n<= x.to_i
+while n <= x
+    if n <= x
     (n == 1)
-       puts ("#" * n).rjust(x.to_i)
+       puts ("#" * n).rjust(x)
     else
         break
     end
    n += 1
 end
+# lovely pyramid goes to infinity if you want !
